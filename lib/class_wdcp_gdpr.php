a76de0643765039975df125ca8175cce0a0d629c
vladislav <ve@incsub.com>
2018-05-21 07:20:15 +0200

    Clean up

--- lib/class_wdcp_gdpr.php
+++ lib/class_wdcp_gdpr.php
@@ -13,14 +13,14 @@
 class Wdcp_Gdpr {
 
 	private function __construct() {}
-	
+
 	public static function serve() {
 		$me = new self;
 		$me->_add_hooks();
 	}
 
 	private function _add_hooks() {
-		add_action('admin_init', array($this, 'add_privacy_policy'));
+		add_action( 'admin_init', array( $this, 'add_privacy_policy' ) );
 
 		add_filter(
 			'wp_privacy_personal_data_exporters',
@@ -41,8 +41,8 @@ class Wdcp_Gdpr {
 	 */
 	public function register_data_exporter( $exporters ) {
 		$exporters['wdcp'] = array(
-			'exporter_friendly_name' => __('Comments Plus metadata', 'wdcp'),
-			'callback' => array($this, 'export_user_metadata'),
+			'exporter_friendly_name' => __( 'Comments Plus metadata', 'wdcp' ),
+			'callback' => array( $this, 'export_user_metadata' ),
 		);
 		return $exporters;
 	}
@@ -66,7 +66,7 @@ class Wdcp_Gdpr {
 	 * Exports plugins metadata
 	 *
 	 * @param string $email User email.
-	 * @param int $page Data page.
+	 * @param int    $page Data page.
 	 *
 	 * @return array
 	 */
@@ -75,25 +75,25 @@ class Wdcp_Gdpr {
 			'data' => array(),
 			'done' => true,
 		);
-		$comment_ids = $this->get_comments_list($email);
-		if (empty($comment_ids)) {
+		$comment_ids = $this->get_comments_list( $email );
+		if ( empty( $comment_ids ) ) {
 			return $result;
 		}
 
-		$label = __('Comments Plus metadata', 'wcp');
+		$label = __( 'Comments Plus metadata', 'wcp' );
 		$exports = array();
 		foreach ( $comment_ids as $cid ) {
-			$raw = get_comment_meta($cid, 'wdcp_comment', true);
-			if (empty($raw)) {
+			$raw = get_comment_meta( $cid, 'wdcp_comment', true );
+			if ( empty( $raw ) ) {
 				continue;
 			}
 
 			$data = array();
-			foreach ($raw as $key => $value) {
-				if (preg_match('/author_id/', $key)) {
-					$key = __('Author ID', 'wdcp');
-				} else if (preg_match('/avatar/', $key)) {
-					$key = __('Avatar', 'wcp');
+			foreach ( $raw as $key => $value ) {
+				if ( preg_match( '/author_id/', $key ) ) {
+					$key = __( 'Author ID', 'wdcp' );
+				} elseif ( preg_match( '/avatar/', $key ) ) {
+					$key = __( 'Avatar', 'wcp' );
 				}
 				$data[] = array(
 					'name' => $key,
@@ -108,15 +108,16 @@ class Wdcp_Gdpr {
 				'data' => $data,
 			);
 		}
+		$result['data'] = $exports;
 
-		return $exports;
+		return $result;
 	}
 
 	/**
 	 * Erases plugins metadata
 	 *
 	 * @param string $email User email.
-	 * @param int $page Data page.
+	 * @param int    $page Data page.
 	 *
 	 * @return array
 	 */
@@ -140,18 +141,18 @@ class Wdcp_Gdpr {
 		);
 		$byid = $byemail = array();
 
-		$user = get_user_by('email', $email);
-		if (!empty($user->ID)) {
+		$user = get_user_by( 'email', $email );
+		if ( ! empty( $user->ID ) ) {
 			$byid = get_comments(
-				wp_parse_args("user_id={$user->ID}", $args)
+				wp_parse_args( "user_id={$user->ID}", $args )
 			);
 		}
 
 		$byemail = get_comments(
-			wp_parse_args("author_email={$email}", $args)
+			wp_parse_args( "author_email={$email}", $args )
 		);
 
-		return array_merge($byid, $byemail);
+		return array_merge( $byid, $byemail );
 	}
 
 	/**
@@ -174,12 +175,12 @@ class Wdcp_Gdpr {
 	 */
 	public function get_policy_content() {
 		return '' .
-			'<h3>' . __('Third parties', 'wdcp') . '</h3>' .
-			'<p>' . __('This site might be using third parties to assert your identity (with your explicit content) before leaving a comment. These services include Facebook, Google and Twitter.', 'wdcp') . '</p>' .
-			'<h3>' . __('Additional data', 'wdcp') . '</h3>' .
-			'<p>' . __('Your comments on this site will be augmented with additional data coming from the selected identity provider. This data includes your name, username, email, avatar and profile URL. This data can be exported and removed.', 'wdcp') . '</p>' .
-			'<h3>' . __('Cookies', 'wdcp') . '</h3>' .
-			'<p>' . __('In addition to standard WordPress comments cookies, this site might be setting an additional cookie to remember your preferred commenting identity provider. This cookie will last for one year.', 'wdcp') . '</p>' .
+			'<h3>' . __( 'Third parties', 'wdcp' ) . '</h3>' .
+			'<p>' . __( 'This site might be using third parties to assert your identity (with your explicit content) before leaving a comment. These services include Facebook, Google and Twitter.', 'wdcp' ) . '</p>' .
+			'<h3>' . __( 'Additional data', 'wdcp' ) . '</h3>' .
+			'<p>' . __( 'Your comments on this site will be augmented with additional data coming from the selected identity provider. This data includes your name, username, email, avatar and profile URL. This data can be exported and removed.', 'wdcp' ) . '</p>' .
+			'<h3>' . __( 'Cookies', 'wdcp' ) . '</h3>' .
+			'<p>' . __( 'In addition to standard WordPress comments cookies, this site might be setting an additional cookie to remember your preferred commenting identity provider. This cookie will last for one year.', 'wdcp' ) . '</p>' .
 		'';
 	}
 }
